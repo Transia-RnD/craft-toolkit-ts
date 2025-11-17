600204b8ebd0e3a84432029d565a902455284e00
Denis Angell <dangell@transia.co>
2025-11-17 08:40:48 +0100

    fix

--- contracts/easymode/src/lib.rs
+++ contracts/easymode/src/lib.rs
@@ -6,7 +6,7 @@ extern crate std;
 
 use xrpl_wasm_std::core::params::function::{get_function_param, safe_get_function_param};
 use xrpl_wasm_std::core::types::account_id::AccountID;
-use xrpl_wasm_std::core::types::amount::token_amount::TokenAmount;
+use xrpl_wasm_std::core::types::amount::Amount;
 use xrpl_wasm_std::host::trace::{trace_num, DataRepr};
 
 const SUCCESS: i32 = 0;
@@ -15,8 +15,10 @@ const BAD_PARAM: i32 = -1;
 #[unsafe(no_mangle)]
 pub extern "C" fn redirect() -> i32 {
 
-    // Get: TokenAmount
-    let amount = match get_function_parameter::<TokenAmount>(0) {
+    // Approve
+
+    // Get: Amount
+    let amount = match get_function_param::<Amount>(0) {
         Ok(a) => a,
         Err(err) => {
             let _ = trace_num("`amount` Parameter Error Code:", err as i64);
